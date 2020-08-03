9d2bcef2952b3bce7f9c93ae8fb655f69ab9fd5b
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-08-03 14:55:03 -0400

    Capitalized property names

--- levels/TopDown.cs
+++ levels/TopDown.cs
@@ -2,6 +2,6 @@ using Godot;
 
 public class TopDown : Node2D
 {
-    [Export] public float friction { get; set; } = 800;
-    [Export] public float drag { get; set; } = 0.1F;
+    [Export] public float Friction { get; set; } = 800;
+    [Export] public float Drag { get; set; } = 0.1F;
 }
