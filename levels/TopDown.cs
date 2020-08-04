a0790c37d6b6cb495dc9aaec0126b3c5a08e1717
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-08-03 22:45:54 -0400

    Corrected Drag value

--- levels/TopDown.cs
+++ levels/TopDown.cs
@@ -3,5 +3,5 @@ using Godot;
 public class TopDown : Node2D
 {
     [Export] public float Friction { get; set; } = 800;
-    [Export] public float Drag { get; set; } = 0.1F;
+    [Export] public float Drag { get; set; } = 0.01F;
 }
