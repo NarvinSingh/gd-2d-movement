07e71653737bf7e7f639f96b4149a9e91aae4ff4
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-08-23 14:43:09 -0400

    Fixes to Graph drawing and crosshair

--- levels/KinematicsTest.cs
+++ levels/KinematicsTest.cs
@@ -10,53 +10,66 @@ public class KinematicsTest : Node2D
 
     public override void _Ready()
     {
+        // lib class tests
+        //GD.Print(Summarize("ApproximateTest.ItCalculatesIsEqual", ApproximateTest.ItCalculatesIsEqual()));
+        //GD.Print(Summarize("ApproximateTest.ItReimannSums", ApproximateTest.ItReimannSums()));
+        //GD.Print(Summarize("ApproximateTest.ItReimannSumsTo", ApproximateTest.ItReimannSumsTo()));
+
+        //GD.Print(Summarize("KinematicsTest.ItCalculatesTimeToStop", Test.KinematicsTest.ItCalculatesTimeToStop()));
+        //GD.Print(Summarize("KinematicsTest.ItCalculatesVelocity", Test.KinematicsTest.ItCalculatesVelocity()));
+        //GD.Print(Summarize(
+        //        "KinematicsTest.ItCalculatesTerminalVelocity", Test.KinematicsTest.ItCalculatesTerminalVelocity()));
+
+        //GD.Print(Summarize("RangeTest.ItInstantiates", RangeTest.ItInstantiates()));
+        //GD.Print(Summarize("RangeTest.ItIncludes", RangeTest.ItIncludes()));
+
+        //GD.Print(Summarize("AxisTest.ItCalcsExtents", AxisTest.ItCalcsExtents()));
+        //GD.Print(Summarize("AxisTest.ItInstantiates", AxisTest.ItInstantiates()));
+        //GD.Print(Summarize("AxisTest.ItMaps", AxisTest.ItMaps()));
+        //GD.Print(Summarize("AxisTest.ItUnmaps", AxisTest.ItUnmaps()));
+
+        // Graph tests
         carousel = (Carousel)GetNode("Carousel");
         graphScene = (PackedScene)ResourceLoader.Load("res://nodes/Graph.tscn");
 
-        // lib class tests
-        GD.Print(Summarize("ApproximateTest.ItCalculatesIsEqual", ApproximateTest.ItCalculatesIsEqual()));
-        GD.Print(Summarize("ApproximateTest.ItReimannSums", ApproximateTest.ItReimannSums()));
-        GD.Print(Summarize("ApproximateTest.ItReimannSumsTo", ApproximateTest.ItReimannSumsTo()));
-
-        GD.Print(Summarize("KinematicsTest.ItCalculatesTimeToStop", Test.KinematicsTest.ItCalculatesTimeToStop()));
-        GD.Print(Summarize("KinematicsTest.ItCalculatesVelocity", Test.KinematicsTest.ItCalculatesVelocity()));
-        GD.Print(Summarize(
-                "KinematicsTest.ItCalculatesTerminalVelocity", Test.KinematicsTest.ItCalculatesTerminalVelocity()));
-
-        GD.Print(Summarize("RangeTest.ItInstantiates", RangeTest.ItInstantiates()));
-        GD.Print(Summarize("RangeTest.ItIncludes", RangeTest.ItIncludes()));
-
-        GD.Print(Summarize("AxisTest.ItCalcsExtents", AxisTest.ItCalcsExtents()));
-        GD.Print(Summarize("AxisTest.ItInstantiates", AxisTest.ItInstantiates()));
-        GD.Print(Summarize("AxisTest.ItMaps", AxisTest.ItMaps()));
-        GD.Print(Summarize("AxisTest.ItUnmaps", AxisTest.ItUnmaps()));
-
-        // Graph tests
-        Graph graph;
-        graph = CreateGraph(new float[][] { new float[] { }, new float[] { } }, "0a) NULL");
-        graph.XAxisLabel = "x";
-        graph.YAxisLabel = "y";
-        carousel.AddNode(graph);
-        graph = CreateGraph(new float[][] { new float[] { 1 }, new float[] { 1 } }, "0b) y = 1");
-        carousel.AddNode(graph);
-        graph = CreateGraph(new float[][] {
+        carousel.AddNode(CreateGraph(new float[][] { new float[] { }, new float[] { } }, "1) NULL", "x", "y"));
+        carousel.AddNode(CreateGraph(new float[][] { new float[] { 1 }, new float[] { 1 } }, "2) (1, 1)", "x", "y"));
+        carousel.AddNode(
+                CreateGraph(new float[][] { new float[] { -1 }, new float[] { -1 } }, "3) (-1, -1)", "x", "y"));
+        carousel.AddNode(CreateGraph(new float[][] { new float[] { 1 }, new float[] { -1 } }, "4) (1, -1)", "x", "y"));
+        carousel.AddNode(CreateGraph(new float[][] { new float[] { -1 }, new float[] { 1 } }, "5) (-1, 1)", "x", "y"));
+        carousel.AddNode(CreateGraph(new float[][] { new float[] { 1, 2 }, new float[] { 1, 2 } }, "6) Q1", "x", "y"));
+        carousel.AddNode(
+                CreateGraph(new float[][] { new float[] { -2, -1 }, new float[] { 1, 2 } }, "7) Q2", "x", "y"));
+        carousel.AddNode(
+                CreateGraph(new float[][] { new float[] { -2, -1 }, new float[] { -2, -1 } }, "8) Q3", "x", "y"));
+        carousel.AddNode(
+                CreateGraph(new float[][] { new float[] { 1, 2 }, new float[] { -2, -1 } }, "9) Q4", "x", "y"));
+        carousel.AddNode(CreateGraph(new float[][] {
+                new float[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                new float[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } },
+                "10) y = 1", "x", "y"));
+        carousel.AddNode(CreateGraph(new float[][] {
+                new float[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new float[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } },
+                "11) x = 1", "x", "y", false));
+        carousel.AddNode(CreateGraph(new float[][] {
                 new float[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
                 new float[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } },
-                "0c) y = x");
-        carousel.AddNode(graph);
-        carousel.AddNode(CreateGraph(GenerateVelocityData(0, 100, 0.01), "1) v0 = 0, a = 100, d = 0.01"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(0, -100, 0.01), "2) v0 = 0, a = -100, d = 0.01"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(200, 100, 0.01), "3) v0 = 200, a = 100, d = 0.01"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(200, -100, 0.01), "4) v0 = 200, a = -100, d = 0.01"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(-200, 100, 0.01), "5) v0 = -200, a = 100, d = 0.01"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(-200, -100, 0.01), "6) v0 = -200, a = -100, d = 0.01"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(0, 100), "7) v0 = 0, a = 100"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(0, -100), "8) v0 = 0, a = -100"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(15, 10), "9) v0 = 15, a = 10"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(15, -10), "10) v0 = 15, a = -10"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(-15, 10), "11) v0 = -15, a = 10"));
-        carousel.AddNode(CreateGraph(GenerateVelocityData(-15, -10), "12) v0 = -15, a = -10"));
-        carousel.SetActiveNode(0);
+                "12) y = x", "x", "y"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(0, 100, 0.01), "13) v0 = 0, a = 100, d = 0.01"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(0, -100, 0.01), "14) v0 = 0, a = -100, d = 0.01"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(200, 100, 0.01), "15) v0 = 200, a = 100, d = 0.01"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(200, -100, 0.01), "16) v0 = 200, a = -100, d = 0.01"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(-200, 100, 0.01), "17) v0 = -200, a = 100, d = 0.01"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(-200, -100, 0.01), "18) v0 = -200, a = -100, d = 0.01"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(0, 100), "19) v0 = 0, a = 100"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(0, -100), "20) v0 = 0, a = -100"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(15, 10), "21) v0 = 15, a = 10"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(15, -10), "22) v0 = 15, a = -10"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(-15, 10), "23) v0 = -15, a = 10"));
+        carousel.AddNode(CreateGraph(GenerateVelocityData(-15, -10), "24) v0 = -15, a = -10"));
+        carousel.SetActiveNode(11);
 
         //GD.Print("Goodbye");
         //GetTree().Quit();
@@ -97,8 +110,8 @@ public class KinematicsTest : Node2D
         return new float[][] { ts, vs };
     }
 
-    private Graph CreateGraph(
-            float[] xSeries, float[] ySeries, string title, string xLabel = "Time", string yLabel = "Velocity")
+    private Graph CreateGraph(float[] xSeries, float[] ySeries, string title, string xLabel = "Time",
+            string yLabel = "Velocity", bool xSnap = true, bool ySnap = true)
     {
         Graph graph = (Graph)graphScene.Instance();
 
@@ -107,13 +120,16 @@ public class KinematicsTest : Node2D
         graph.YAxisLabel = yLabel;
         graph.XSeries = xSeries;
         graph.YSeries = ySeries;
+        graph.XSnap = xSnap;
+        graph.YSnap = ySnap;
 
         return graph;
     }
 
-    private Graph CreateGraph(float[][] data, string title, string xLabel = "Time", string yLabel = "Velocity")
+    private Graph CreateGraph(float[][] data, string title, string xLabel = "Time", string yLabel = "Velocity",
+            bool xSnap = true, bool ySnap = true)
     {
         if (data.Length < 2) throw new ArgumentOutOfRangeException("data must contain 2 array elements.");
-        return CreateGraph(data[0], data[1], title, xLabel, yLabel);
+        return CreateGraph(data[0], data[1], title, xLabel, yLabel, xSnap, ySnap);
     }
 }
--- nodes/Graph.cs
+++ nodes/Graph.cs
@@ -3,44 +3,47 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class Graph : Area2D
+public class Graph : Node2D
 {
     private const string DEFAULT_POINT_HIGHLIGHT_COLOR = "6680FF";
     private const float POINT_SCALE = 1.2F;
     private const float CHI_OFFSET = 6;
 
-    private bool isReady;
     private string title;
     private string xAxisLabel;
     private string yAxisLabel;
     private Label titleLabel;
+    private Area2D plot;
     private Node2D xAxis;
     private Line2D xLine;
     private Label xLabel;
     private Label xMinLabel;
     private Label xMaxLabel;
-    private float xAxisLen;
     private Node2D yAxis;
     private Line2D yLine;
     private Label yLabel;
     private Label yMinLabel;
     private Label yMaxLabel;
-    private float yAxisLen;
-    private Vector2 origin;
     private Node2D crosshair;
     private Line2D xCrosshair;
     private Line2D yCrosshair;
     private Label crosshairInfo;
+    private Vector2 origin;
+    private Axis axisX;
+    private Axis axisY;
+    private float xAxisLen;
+    private float yAxisLen;
     private float chiExpMgnTop;
     private float chiExpMgnLeft;
     private float chiExpMgnBtm;
     private float chiExpMgnRight;
     private bool isCrosshairActive;
+    private bool isReady;
+    private List<Sprite> points;
+    private Sprite activePoint;
     private PackedScene point;
     private Color pointColor;
-    private List<Sprite> points;
     private Vector2 pointScale;
-    private Sprite activePoint;
 
     [Export]
     public string Title
@@ -88,34 +91,39 @@ public class Graph : Area2D
 
     [Export] public float[] YSeries { get; set; }
 
+    [Export] public bool XSnap { get; set; } = false;
+
+    [Export] public bool YSnap { get; set; } = true;
+
     [Export] public Color HighlightColor { get; set; } = new Color(DEFAULT_POINT_HIGHLIGHT_COLOR);
 
     public override void _Ready()
     {
         isReady = true;
 
-        // Get child nodes
+        //Get child nodes
         titleLabel = (Label)GetNode("Title");
-        xAxis = (Node2D)GetNode("XAxis");
-        xLine = (Line2D)GetNode("XAxis/Axis");
-        xLabel = (Label)GetNode("XAxis/Label");
-        xMinLabel = (Label)GetNode("XAxis/Min");
-        xMaxLabel = (Label)GetNode("XAxis/Max");
-        yAxis = (Node2D)GetNode("YAxis");
-        yLine = (Line2D)GetNode("YAxis/Axis");
-        yLabel = (Label)GetNode("YAxis/Label");
-        yMinLabel = (Label)GetNode("YAxis/Min");
-        yMaxLabel = (Label)GetNode("YAxis/Max");
-        crosshair = (Node2D)GetNode("Crosshair");
-        xCrosshair = (Line2D)GetNode("Crosshair/XCrosshair");
-        yCrosshair = (Line2D)GetNode("Crosshair/YCrosshair");
-        crosshairInfo = (Label)GetNode("Crosshair/Info");
+        plot = (Area2D)GetNode("Plot");
+        xAxis = (Node2D)GetNode("Plot/XAxis");
+        xLine = (Line2D)GetNode("Plot/XAxis/Axis");
+        xLabel = (Label)GetNode("Plot/XAxis/Topmost/Label");
+        xMinLabel = (Label)GetNode("Plot/XAxis/Topmost/Min");
+        xMaxLabel = (Label)GetNode("Plot/XAxis/Topmost/Max");
+        yAxis = (Node2D)GetNode("Plot/YAxis");
+        yLine = (Line2D)GetNode("Plot/YAxis/Axis");
+        yLabel = (Label)GetNode("Plot/YAxis/Topmost/Label");
+        yMinLabel = (Label)GetNode("Plot/YAxis/Topmost/Min");
+        yMaxLabel = (Label)GetNode("Plot/YAxis/Topmost/Max");
+        crosshair = (Node2D)GetNode("Plot/Crosshair");
+        xCrosshair = (Line2D)GetNode("Plot/Crosshair/XLine");
+        yCrosshair = (Line2D)GetNode("Plot/Crosshair/YLine");
+        crosshairInfo = (Label)GetNode("Plot/Crosshair/Info");
         StyleBoxFlat chiStyleBox = (StyleBoxFlat)crosshairInfo.GetStylebox("normal");
 
         // Initialize class variables
-        origin = xLine.Points[0];
-        xAxisLen = origin.DistanceTo(xLine.Points[1]);
-        yAxisLen = origin.DistanceTo(yLine.Points[1]);
+        origin = plot.Position;
+        xAxisLen = xLine.Points[0].DistanceTo(xLine.Points[1]);
+        yAxisLen = yLine.Points[0].DistanceTo(yLine.Points[1]);
         chiExpMgnTop = chiStyleBox.ExpandMarginTop;
         chiExpMgnLeft = chiStyleBox.ExpandMarginLeft;
         chiExpMgnBtm = chiStyleBox.ExpandMarginBottom;
@@ -139,67 +147,70 @@ public class Graph : Area2D
     {
         Sprite newPoint = (Sprite)point.Instance();
         newPoint.Position = new Vector2(x, y);
-        AddChild(newPoint);
+        plot.AddChild(newPoint);
         return newPoint;
-    }
-
-    private void Erase()
-    {
-        if (points is null) return;
-        points.ForEach((point) => point.QueueFree());
-        points.Clear();
     }
 
     private void Draw()
     {
         if (!isReady || XSeries is null || YSeries is null || XSeries.Length != YSeries.Length) return;
 
-        Erase();
+        // Destroy the current points
+        if (!(points is null))
+        {
+            points.ForEach((point) => point.QueueFree());
+            points.Clear();
+        }
 
         int frames = XSeries.Length;
-        Axis x;
-        Axis y;
 
         // Graph has data
         if (XSeries.Length > 0)
         {
-            x = new Axis(origin.x, xAxisLen, XSeries);
-            y = new Axis(origin.y - yAxisLen, yAxisLen, YSeries, true);
+            axisX = new Axis(0, xAxisLen, XSeries);
+            axisY = new Axis(0, yAxisLen, YSeries, true);
         }
         // Graph has no data so use default extents
         else
         {
-            x = new Axis(origin.x, xAxisLen, 0, 1);
-            y = new Axis(origin.y - yAxisLen, yAxisLen, 0, 1, true);
+            axisX = new Axis(0, xAxisLen, -1, 1);
+            axisY = new Axis(0, yAxisLen, -1, 1, true);
         }
 
-        xMinLabel.Text = String.Format("{0:F2}", x.LowerExtent);
-        xMaxLabel.Text = String.Format("{0:F2}", x.UpperExtent);
-        yMinLabel.Text = String.Format("{0:F2}", y.LowerExtent);
-        yMaxLabel.Text = String.Format("{0:F2}", y.UpperExtent);
+        // Both axes would be scrolled out of view based on their extents, so adjust the x extents to show the y-axis
+        if ((axisY.LowerExtent > 0 || axisY.UpperExtent < 0) && (axisX.LowerExtent > 0 || axisX.UpperExtent < 0))
+        {
+            if (axisX.LowerExtent > 0) axisX.LowerExtent = 0;
+            else axisX.UpperExtent = 0;
+        }
 
+        // The x-axis would be below the lower extent or above the upper extent of the y-axis, so hide it
+        if (axisY.LowerExtent > 0 || axisY.UpperExtent < 0) xAxis.Visible = false;
         // Translate the x-axis to 0 on the y-axis
-        if (y.LowerExtent != 0)
-        {
-            // The x-axis would be below the lower extent or above the upper extent of the y-axis, so hide it
-            if (y.LowerExtent > 0 || y.UpperExtent < 0) xAxis.Visible = false;
-            // The x-axis starts off at origin.y so translate it back to screen 0, then translate it to the screen
-            // coordinates of 0 on the y-axis.
-            else xAxis.Translate(new Vector2(0, -origin.y + y.Map(0)));
-        }
+        else xAxis.Position = new Vector2(xAxis.Position.x, axisY.Map(0));
 
+        // The y-axis would be below the lower extent or above the upper extent of the x-axis, so hide it
+        if (axisX.LowerExtent > 0 || axisX.UpperExtent < 0) yAxis.Visible = false;
         // Translate the y-axis to 0 on the x-axis
-        if (x.LowerExtent != 0)
-        {
-            // The y-axis would be below the lower extent or above the upper extent of the x-axis, so hide it
-            if (x.LowerExtent > 0 || x.UpperExtent < 0) yAxis.Visible = false;
-            // The y-axis starts off at origin.x so translate it back to screen 0, then translate it to the screen
-            // coordinates of 0 on the x-axis.
-            else yAxis.Translate(new Vector2(-origin.x + x.Map(0), 0));
-        }
-        else xMinLabel.Visible = false;
+        else yAxis.Position = new Vector2(axisX.Map(0), yAxis.Position.y);
 
-        for (int i = 0; i < frames; i++) points.Add(CreatePoint(x.Map(XSeries[i]), y.Map(YSeries[i])));
+        // First make sure all the min/max labels are visible
+        xMinLabel.Visible = true;
+        xMaxLabel.Visible = true;
+        yMinLabel.Visible = true;
+        yMaxLabel.Visible = true;
+
+        // Now hide a min or max label if the axes intersect on screen so two labels don't butt up against each other
+        if (axisX.LowerExtent == 0) xMinLabel.Visible = false;
+        else if (axisY.LowerExtent == 0) yMinLabel.Visible = false;
+        else if (axisX.UpperExtent == 0 && axisY.UpperExtent == 0) xMaxLabel.Visible = false;
+
+        xMinLabel.Text = String.Format("{0:F2}", axisX.LowerExtent);
+        xMaxLabel.Text = String.Format("{0:F2}", axisX.UpperExtent);
+        yMinLabel.Text = String.Format("{0:F2}", axisY.LowerExtent);
+        yMaxLabel.Text = String.Format("{0:F2}", axisY.UpperExtent);
+
+        for (int i = 0; i < frames; i++) points.Add(CreatePoint(axisX.Map(XSeries[i]), axisY.Map(YSeries[i])));
     }
 
     private void SetActivePoint(int i)
@@ -231,52 +242,115 @@ public class Graph : Area2D
         isCrosshairActive = isActiveFixed;
         crosshair.Visible = isActiveFixed;
         if (!isCrosshairActive) SetActivePoint(-1);
+    }
 
+    private int GetSnapIndex(float[] series, float value)
+    {
+        float minDistance = float.PositiveInfinity;
+        int seriesLength = series.Length;
+        int index = 0;
+
+        for (int i = 0; i < seriesLength; i++)
+        {
+            float dist = series[i] - value;
+
+            if (dist < 0) dist = -dist;
+            if (dist < minDistance)
+            {
+                minDistance = dist;
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    private int GetSnapIndex(Vector2 position)
+    {
+        float minDistSq = float.PositiveInfinity;
+        int seriesLength = XSeries.Length;
+        int index = 0;
+
+        for (int i = 0; i < seriesLength; i++)
+        {
+            float distSq = position.DistanceSquaredTo(new Vector2(axisX.Map(XSeries[i]), axisY.Map(YSeries[i])));
+
+            if (distSq < minDistSq)
+            {
+                minDistSq = distSq;
+                index = i;
+            }
+        }
+
+        return index;
     }
 
     private void PlaceCrosshair(Vector2 position)
     {
-        Vector2 chiPosition = position;
+        Vector2 plotPos = position - origin; // translate position relative to window back to plot origin
 
         // Graph has data
         if (XSeries.Length > 0)
         {
             int i;
             float yCrosshairPosX;
+            float xCrosshairPosY;
 
             // Graph has more than one point, so get the point from the cursor x position and snap
             if (XSeries.Length > 1)
             {
-                i = Math.Min(
-                    Math.Max(Convert.ToInt32((XSeries.Length - 1) * (position.x - origin.x) / xAxisLen), 0),
-                    XSeries.Length - 1);
-                yCrosshairPosX = i * xAxisLen / (XSeries.Length - 1);
+                // Only x-snap, so get the closest point along x
+                if (XSnap && !YSnap) i = GetSnapIndex(XSeries, axisX.Unmap(plotPos.x));
+                // Only y-snap, so get the closest point along y
+                else if (!XSnap && YSnap) i = GetSnapIndex(YSeries, axisY.Unmap(plotPos.y));
+                // x and y snap or no snap, so get the closest point along either direction
+                else i = GetSnapIndex(plotPos);
+
+                //i = Math.Min(
+                //    Math.Max(Convert.ToInt32((XSeries.Length - 1) * plotPos.x / xAxisLen), 0),
+                //    XSeries.Length - 1);
+                //yCrosshairPosX = XSnap ? i * xAxisLen / (XSeries.Length - 1) : plotPos.x;
+                //xCrosshairPosY = YSnap ? -i * yAxisLen / (YSeries.Length - 1) : cursorPosY;
+                //xCrosshairPosY = YSnap ? axisY.Map(YSeries[i]) - origin.y : plotPos.y;
             }
             // Graph has only one point in the middle of itself
             else
             {
                 i = 0;
-                yCrosshairPosX = xAxisLen / 2;
+                //yCrosshairPosX = XSnap ? xAxisLen / 2 : plotPos.x;
+                //xCrosshairPosY = YSnap ? -yAxisLen / 2 : plotPos.y;
             }
+
+            yCrosshairPosX = XSnap ? axisX.Map(XSeries[i]) : plotPos.x;
+            xCrosshairPosY = YSnap ? axisY.Map(YSeries[i]) : plotPos.y;
 
             // Set crosshair info text to info about the point, set the crosshair position and active point
             crosshairInfo.Text = String.Format("{0,8}: {1,6}\n{2,8}: {3,6}",
-                    xAxisLabel, i >= 0 && i < XSeries.Length ? String.Format("{0:F2}", XSeries[i]) : "N/A",
-                    yAxisLabel, i >= 0 && i < YSeries.Length ? String.Format("{0:F2}", YSeries[i]) : "N/A");
-            xCrosshair.Position = new Vector2(xCrosshair.Position.x, position.y - origin.y);
+                    //xAxisLabel, i >= 0 && i < XSeries.Length ? String.Format("{0:F2}", XSeries[i]) : "N/A",
+                    xAxisLabel, String.Format("{0:F2}", XSeries[i]),
+                    //yAxisLabel, i >= 0 && i < YSeries.Length ? String.Format("{0:F2}", YSeries[i]) : "N/A");
+                    yAxisLabel, String.Format("{0:F2}", YSeries[i]));
+            xCrosshair.Position = new Vector2(xCrosshair.Position.x, xCrosshairPosY);
             yCrosshair.Position = new Vector2(yCrosshairPosX, yCrosshair.Position.y);
             SetActivePoint(i);
         }
         // Graph has no data
         else
         {
-            // Set crosshair info text to info about the cursor, set the crosshair position and active point
-            xCrosshair.Position = new Vector2(xCrosshair.Position.x, position.y - origin.y);
-            yCrosshair.Position = new Vector2(position.x - origin.x, yCrosshair.Position.y);
-            crosshairInfo.Text = String.Format("Cursor: {0}", position);
+            // Set crosshair info text to info about the cursor, and set the crosshair position
+            xCrosshair.Position = new Vector2(xCrosshair.Position.x, plotPos.y);
+            yCrosshair.Position = new Vector2(plotPos.x, yCrosshair.Position.y);
+            crosshairInfo.Text = String.Format("Cursor: {0}\nPoint:  ({1:F2}, {2:F2})",
+                    plotPos, axisX.Unmap(plotPos.x), axisY.Unmap(plotPos.y));
         }
 
+        crosshairInfo.Text = String.Format("{0}\n-DEBUG-\nWindow Pos: {1}\nPlot Pos:   ({2}, {3})",
+                crosshairInfo.Text, position, plotPos.x, plotPos.y);
+
         // Crosshair info position is set now because it depends on the text that was set above
+        //Vector2 chiPosition = position;
+        Vector2 chiPosition = new Vector2(yCrosshair.Position.x + origin.x, xCrosshair.Position.y + origin.y);
+
         if (position.x + crosshairInfo.RectSize.x + chiExpMgnLeft + chiExpMgnRight + CHI_OFFSET <= origin.x + xAxisLen)
         {
             chiPosition.x += chiExpMgnLeft + CHI_OFFSET;
@@ -301,6 +375,15 @@ public class Graph : Area2D
             if (isCrosshairActive) PlaceCrosshair(mouseClick.Position);
         }
         else if (@event is InputEventMouseMotion mouseMove && isCrosshairActive) PlaceCrosshair(mouseMove.Position);
+        else if (@event is InputEventMouseButton mouseClick2 && mouseClick2.ButtonIndex == (int)ButtonList.Right && mouseClick2.Pressed)
+        {
+            //XSnap = !XSnap;
+            //YSnap = !YSnap;
+            //GD.Print("Toggle snap ", XSnap, YSnap);
+            XSeries = new float[] { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+            YSeries = new float[] { 25, 16, 9, 4, 1, 0, 1, 4, 9, 16, 25 };
+            Draw();
+        }
     }
 
     private void HandleMouseExited()
